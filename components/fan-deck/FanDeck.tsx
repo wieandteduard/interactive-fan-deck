375a4d57f31df6e19fb2f9764ec4f19300a45ebb
Eduard Wieandt <studio@eduardwieandt.com>
2026-09-14 13:28:07 +0200

    A touch quieter

--- components/fan-deck/FanDeck.tsx
+++ components/fan-deck/FanDeck.tsx
@@ -240,7 +240,7 @@ export function FanDeck() {
   });
   const sound = useDialKit("Sound", {
     on: true,
-    volume: [0.14, 0, 1, 0.02],
+    volume: [0.1, 0, 1, 0.02],
     voice: { type: "select", options: [...VOICINGS], default: "tap" },
     /* multiplier on brightness (the lowpass ceilings) */
     bright: [1, 0.4, 2, 0.05],
